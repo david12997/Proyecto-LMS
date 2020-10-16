92bead2afdc11398b2bd1029d4b7a8bc258fd3f8
Jose David Castañeda <51899338+david12997@users.noreply.github.com>
2020-10-15 23:47:15 -0500

    pruebas

--- presentacion/estudiante/miscursos.php
+++ presentacion/estudiante/miscursos.php
@@ -53,7 +53,7 @@ include_once '../../librerias/propias/mostrar-errores.php';
 
 
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a class="navbar-brand" href="../../index.php"><img id="logo" src="../../assets/img/logo-tutorias-final.png" alt=""></a>
+          <a class="navbar-brand" href="index.php"><img id="logo" src="../../assets/img/logo-tutorias-final.png" alt=""></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
